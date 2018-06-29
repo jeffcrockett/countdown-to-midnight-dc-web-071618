59a11438183fb7e312a53eb1f4b003a2c7c57b0e
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-29 21:26:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,12 @@
 #write your code here
-
 def countdown(count)
+  while count > 0
+    puts count == 1 ? "#{} SECOND!" : "#{count} SECONDS!"
+    count -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(count)
   while count > 0
     puts count == 1 ? "#{} SECOND!" : "#{count} SECONDS!"
     count -= 1
